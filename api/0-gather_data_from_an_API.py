babacc1b7fe0c6272353f01e5a8580fd1c854956
issaevis <evis.topollaj@gmail.com>
2023-08-22 14:43:04 +0200

    Fixed 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,9 +2,9 @@
 '''using API to get data'''
 
 
-import sys
-import requests
 import json
+import requests
+import sys
 
 
 if __name__ == "__main__":
@@ -32,7 +32,7 @@ if __name__ == "__main__":
                 done_tasks_counter += 1
             all_count += 1
 
-    print("Employee {} is done with tasks ({}/{})".format
+    print("Employee {} is done with tasks ({}/{}):".format
           (user["name"], done_tasks_counter, all_count))
     for task in done_tasks:
         print("\t {}".format(task))
