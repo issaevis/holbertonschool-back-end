04ed3af444abade11027657d61a181f29fab9a5f
issaevis <evis.topollaj@gmail.com>
2023-08-26 17:21:32 +0200

    Added a fix to 2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
                 "username": user["username"]
             })
 
-    filename = "{}".format(user["id"]) + ".csv"
+    filename = "{}".format(user["id"]) + ".json"
 
     with open(filename, "w", newline="") as f:
         json.dump({str(user["id"]): user_tasks}, f, indent=2)
